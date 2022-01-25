64a33021befca706b2c60644c83e4eb98dd2c31b
Di Wu <dw323@cornell.edu>
2022-01-24 16:27:03 -0800

    f

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ mkdir -p data
 docker run --name gnosis-fullnode -d \
         --restart unless-stopped \
         --stop-timeout 30 \
-        -p 8180:8180 -p 8645:8645 -p 8646:8646 -p 30304:30304 -p 30304:30304/udp \
+        -p 8645:8645 -p 8646:8646 -p 30304:30304 -p 30304:30304/udp \
         -v $PWD/data:/opt/openethereum/data $IMAGE \
         --chain xdai \
         --base-path '/opt/openethereum/data' \
